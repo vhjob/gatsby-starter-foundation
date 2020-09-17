2e99a8caf78ae985de9a486df73e972791ed9904
vhjob <vhjob@yandex.ru>
2020-09-17 16:42:32 +0300

    promotext on home page

--- src/components/blog-list-home.js
+++ src/components/blog-list-home.js
@@ -32,6 +32,7 @@ export default function BlogListHome() {
                   date(formatString: "MMMM DD, YYYY")
                   slug
                   title
+                  description
                   featuredImage {
                     childImageSharp {
                       fluid(maxWidth: 540, maxHeight: 360, quality: 80) {
